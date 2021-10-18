3baa21b02e71d8731f5027fd8adb6da0e878369e
lguzman <luis.guzman@datumredsoft.com>
2021-10-18 11:12:28 -0600

    Update sso.config.ts

--- src/app/sso.config.ts
+++ src/app/sso.config.ts
@@ -5,7 +5,7 @@ import { AuthConfig } from 'angular-oauth2-oidc';
     issuer: 'https://idsvr4.azurewebsites.net',
 
     // URL of the SPA to redirect the user to after login
-    redirectUri: window.location.origin + '/index.html',
+    redirectUri: window.location.origin + '/#',
 
     // The SPA's id. The SPA is registerd with this id at the auth-server
     // clientId: 'server.code',
